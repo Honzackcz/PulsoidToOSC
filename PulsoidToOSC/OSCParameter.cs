dcba619c1dc5b67b0404ce62a642a09f1e413ce8
Honzackcz <76910833+Honzackcz@users.noreply.github.com>
2024-07-31 23:24:53 +0200

    Added OSC parameter class

---
+++ PulsoidToOSC/OSCParameter.cs
@@ -0,0 +1,25 @@
+﻿using SharpOSC;
+
+namespace PulsoidToOSC
+{
+	public class OSCParameter
+	{
+		public enum Types { Integer, Float, Float01, BoolToggle }
+		public Types Type { get; set; } = Types.Integer;
+		public string Name { get; set; } = string.Empty;
+
+		public OscMessage? GetOscMessage(string oscPath, int heartRate, bool hbToggle)
+		{
+			if (Name == string.Empty || oscPath == string.Empty) return null;
+
+			return Type switch
+			{
+				Types.Integer => new(oscPath + Name, heartRate),
+				Types.Float => new(oscPath + Name, (heartRate / 127f) - 1f),
+				Types.Float01 => new(oscPath + Name, heartRate / 255f),
+				Types.BoolToggle => new(oscPath + Name, hbToggle),
+				_ => null
+			};
+		}
+	}
+}
