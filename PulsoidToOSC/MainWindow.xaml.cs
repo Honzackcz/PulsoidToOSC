5711d6a99584df54539564edf58bf32cf89ed8d9
Honzackcz <76910833+Honzackcz@users.noreply.github.com>
2024-08-02 18:35:33 +0200

    fix for main window being able to maximize

--- PulsoidToOSC/MainWindow.xaml.cs
+++ PulsoidToOSC/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System.Windows;
-using System;
+using System.Windows.Interop;
 
 namespace PulsoidToOSC
 {
@@ -11,6 +11,27 @@ namespace PulsoidToOSC
 		public MainWindow()
 		{
 			InitializeComponent();
+			SourceInitialized += MainWindow_SourceInitialized;
+		}
+
+		private void MainWindow_SourceInitialized(object? sender, EventArgs e)
+		{
+			HwndSource hwndSource = (HwndSource)PresentationSource.FromVisual(this);
+			hwndSource.AddHook(WndProc);
+		}
+
+		private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			const int WM_SYSCOMMAND = 0x112;
+			const int SC_MAXIMIZE = 0xF030;
+
+			if (msg == WM_SYSCOMMAND && wParam.ToInt32() == SC_MAXIMIZE)
+			{
+				handled = true;
+				return IntPtr.Zero;
+			}
+
+			return IntPtr.Zero;
 		}
 	}
 }
