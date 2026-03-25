7c0a457c396d2867ddc94bf23718f2d9ab3960e4
Honzackcz <76910833+Honzackcz@users.noreply.github.com>
2026-03-25 22:45:09 +0100

    updated app version number to 0.5.0

--- PulsoidToOSC/MainProgram.cs
+++ PulsoidToOSC/MainProgram.cs
@@ -5,7 +5,7 @@ namespace PulsoidToOSC
 {
 	internal static class MainProgram
 	{
-		public const string AppVersion = "v0.4.0";
+		public const string AppVersion = "v0.5.0";
 		public const string GitHubOwner = "Honzackcz";
 		public const string GitHubRepo = "PulsoidToOSC";
 
