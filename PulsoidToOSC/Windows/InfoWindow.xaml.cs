bdd1c16dc86f6b73b8b6d35d1f0da987fc9f1121
Honzackcz <76910833+Honzackcz@users.noreply.github.com>
2026-04-06 12:15:02 +0200

    file cleanup

---
+++ PulsoidToOSC/Windows/InfoWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PulsoidToOSC
+{
+	/// <summary>
+	/// Interakční logika pro InfoWindow.xaml
+	/// </summary>
+	public partial class InfoWindow : Window
+	{
+		public InfoWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
